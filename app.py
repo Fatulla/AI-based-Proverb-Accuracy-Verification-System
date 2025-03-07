ec148f826a3229877f2f31c770262b948feb93bb
Fatulla <aliyevfatulla99@gmail.com>
2025-03-07 12:26:24 +0400

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def load_lottie_url(url: str):
         return None
     return r.json()
 
-url = "https://github.com/Fatulla/AILAB_TASK02_Atalar_Sozlari/blob/main/atalar_sozleri.xlsx"
+url = "https://raw.github.com/Fatulla/AILAB_TASK02_Atalar_Sozlari/blob/main/atalar_sozleri.xlsx"
 df = pd.read_excel(url)
 
 # Başlıq hissəsini düzəldirik
